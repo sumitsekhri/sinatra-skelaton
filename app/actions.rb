de718c887547b7935566572c13e10adce17a8efc
Sumit Sekhri <sumitsekhri@gmail.com>
2015-08-10 06:42:27 +0000

    Commit #2 - adding validations

--- app/actions.rb
+++ app/actions.rb
@@ -12,12 +12,20 @@ get '/messages/new' do
   erb :'messages/new'
 end
 
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
 post '/messages' do
   @message = Message.new(
     title:   params[:title],
     content: params[:content],
     author:  params[:author]
   )
-  @message.save
-  redirect '/messages'
+  if @message.save
+    redirect '/messages'
+  else
+    erb :"messages/new"
+  end
 end
